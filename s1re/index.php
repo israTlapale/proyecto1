2d404f118649ff2ae3c67e31c00f31b37bbfbcbe
israTlapale <tlapalegi@gmail.com>
2023-09-08 02:09:24 -0600

    Descripción de los cambios

--- s1re/index.php
+++ s1re/index.php
@@ -27,14 +27,14 @@
                     <div class="titulo-campo">
                         <label for="usuario">Usuario</label>
                     </div>
-                    <div class="contenedor-campo">    
+                    <div class="contenedor-campo">
                         <span class="icono-contenedor">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 icono">
                                 <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
                             </svg>
                         </span>
                         <input class="dato" type="text" name="usuario" id="usuario" placeholder="Usuario">
-                    </div>                    
+                    </div>
                     <div class="titulo-campo">
                         <label for="contraseña">Contraseña</label>
                     </div>
@@ -52,18 +52,19 @@
                 </form>
                 <div class="restaurar-contraseña">
                     <a href="#">Restaurar contraseña</a>
-                </div>                
-            </div>       
+                </div>
+            </div>
         </section>
     </main>
-        
+
     <footer>
         <div class="pie-pagina">
             <p>Fecha Mod.: 15/08/2023 Versión: 1.16.0 Ambiente: Production</p>
         </div>
     </footer>
 
-    
-    <script src="js/validacion.js"></script>        
+
+    <script src="js/validacion.js"></script>
 </body>
+
 </html>
